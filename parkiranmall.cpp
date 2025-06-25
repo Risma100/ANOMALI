9fd81c6dfc3c19f0644c2db72ac75692ece4357c
Risma100 <risma2006.rm@gmail.com>
2025-06-25 23:12:13 +0800

    Feat: Tambah fungsi tambahRiwayatMasuk dan tambahRiwayatKeluar

--- parkiranmall.cpp
+++ parkiranmall.cpp
@@ -29,3 +29,35 @@ void clearScreen() {
 void toLowerCase(char str[]) {
     for (int i = 0; str[i]; i++) str[i] = tolower(str[i]);
 }
+
+void tambahRiwayatMasuk(Kendaraan*& riwayat, const char* plat, const char* jenis, int jamMasuk) {
+    Kendaraan* baru = new Kendaraan;
+    strcpy(baru->plat, plat);
+    strcpy(baru->jenis, jenis);
+    baru->jamMasuk = jamMasuk;
+    baru->next = nullptr;
+
+    if (riwayat == nullptr) riwayat = baru;
+    else {
+        Kendaraan* temp = riwayat;
+        while (temp->next != nullptr) temp = temp->next;
+        temp->next = baru;
+    }
+}
+
+void tambahRiwayatKeluar(RiwayatKeluar*& riwayat, const char* plat, const char* jenis, int jamMasuk, int jamKeluar, int biaya) {
+    RiwayatKeluar* baru = new RiwayatKeluar;
+    strcpy(baru->plat, plat);
+    strcpy(baru->jenis, jenis);
+    baru->jamMasuk = jamMasuk;
+    baru->jamKeluar = jamKeluar;
+    baru->biaya = biaya;
+    baru->next = nullptr;
+
+    if (riwayat == nullptr) riwayat = baru;
+    else {
+        RiwayatKeluar* temp = riwayat;
+        while (temp->next != nullptr) temp = temp->next;
+        temp->next = baru;
+    }
+}
